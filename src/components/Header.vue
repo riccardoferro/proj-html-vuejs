41130a689fd7a19094da6b5512c0e394e9ee01a5
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-26 18:41:22 +0200

    completed header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <header>
         <div class="container-fluid">
-            <div class="row">
-                <img src="../../src/assets/img/logo-img-01.png" alt="image logo">
+            <div class="row justify-content-between">
+                <img class="col-2" src="../../src/assets/img/logo-img-01.png" alt="image logo">
+                <div class="buttons-rf col-auto">
+                    <button v-for="(text,index) in data" :key="index" class="btn">
+                        {{text}} 
+                    </button>
+                <img class="ps-2" src="../../src/assets/svg/svg-1.svg" alt="image logo">
+
+                </div>
+
             </div>
         </div>
     </header>
@@ -22,10 +30,18 @@ export default {
 <style lang="scss" scoped>
     header {
       .row {
-          padding: 20px 40px;
+            padding: 20px 20px;
            img {
-                max-width: 75px;
+                max-width: 80px;
+                max-height: 20px;
+                align-self: center;
             }
+            button {
+              margin: 0 15px;
+              font-size: 12px;
+              font-weight: 400;
+            }
+         
       }
     }
 </style>
