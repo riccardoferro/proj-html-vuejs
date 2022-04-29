38992372609c13c0f4a76cad269d0e6203522fd3
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-29 10:59:55 +0200

    insert icons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
     <main>
         <!-- first part of the main -->
-        <div class="container w-75 m-auto mt-5 mb-5">
-            <div class="row w-75 m-auto pt-5 pb-5 justify-content-between">
+        <div class="container-fluid m-auto mt-5 mb-5 position-relative">
+            <div class="row w-50 m-auto pt-5 pb-5 justify-content-between">
                 <div class="col-4 pe-4">
                     <h4> What we do?</h4>
                     <p class="fs-10-rf color-sc-txt-rf pt-2 pb-4">Lorem ipsum dolor sit amet aenean consectetuer socils adipiscing elit. Aenean commodo ligula eget dolor pretium quis sem.</p>
@@ -43,6 +43,14 @@
                    </div>
                 </div>
             </div>
+        <div class="icons text-center">
+              <i class="fa-brands fa-instagram-square rounded-circle back-color-inst"></i>
+              <i class="fa-brands fa-facebook-f rounded-circle back-color-face"></i>
+              <i class="fa-brands fa-twitter rounded-circle back-color-twitt"></i>
+              <i class="fa-brands fa-pinterest-p rounded-circle back-color-pint"></i>
+            </div>
+        <a href="#" class="arrow-up-rf pos-arrow-2-rf"> </a>
+
         </div>
          <!-- second part of the main -->
         <div class="container-fluid position-relative">
@@ -58,56 +66,63 @@
                 <div class="col-1 back-img-r2 h-300px"></div>
             </div>
             <img src="../../src/assets/img/main-home-video-button-img-01.png" alt="" class="position-absolute top-50 start-50 translate-middle w-50px ">
+        <a href="#" class="arrow-up-rf pos-arrow-rf"> </a>
+        
         </div>
 
         <!-- Third part of the main -->
-        <div class="container-fluid back-img-3-rf pb-5 pt-5">
-            <div class="row w-75 justify-content-evenly m-auto pt-5 mb-5 pb-5">
-                <div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf borders-colors-button1-rf">
-                    <h6>Basic</h6> 
-                    <div class="position-relative">
-                        <h3 class="d-inline-block">99</h3>
-                        <span class="dollar-pos">$</span>
-                    </div>
-                    <p class="fs-10-rf lh-lg color-sc-txt-rf mb-4">
-                        5 design concepts <br>
-                        Full copyright ownership <br>
-                        Art direction <br>
-                        Top level creatives <br>
-                        No money back 
-                    </p>
-                    <button class="btn-rf fs-10-rf rounded-pill mb-5"> Learn more <i class="fa-solid fa-caret-right align-middle"></i>  </button>
-                </div><div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf back-color-img-rf">
-                    <h6>Standard</h6>           
-                    <div class="position-relative">
-                        <h3 class="d-inline-block">119</h3>
-                        <span class="dollar-pos">$</span>
-                    </div>
-                    <p class="fs-10-rf lh-lg color-sc-txt-rf mb-4">
-                        10 design concepts <br>
-                        Full copyright ownership <br>
-                        Art direction <br>
-                        Top level creatives <br>
-                        No money back 
-                    </p>
-                    <button class="btn-rf rounded-pill back-color-img-rf mb-5 fs-10-rf"> Learn more <i class="fa-solid fa-caret-right align-middle"></i> </button>
+        <div class="container-fluid back-img-3-rf pb-5 pt-5 position-relative">
+                <div class="row w-75 justify-content-evenly m-auto pt-5 mb-5 pb-5">
+                        <div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf borders-colors-button1-rf">
+                            <h6>Basic</h6> 
+                            <div class="position-relative">
+                                <h3 class="d-inline-block">99</h3>
+                                <span class="dollar-pos">$</span>
+                            </div>
+                            <p class="fs-10-rf lh-lg color-sc-txt-rf mb-4">
+                                5 design concepts <br>
+                                Full copyright ownership <br>
+                                Art direction <br>
+                                Top level creatives <br>
+                                No money back 
+                            </p>
+                            <button class="btn-rf fs-10-rf rounded-pill mb-5"> Learn more <i class="fa-solid fa-caret-right align-middle"></i>  </button>
+                        </div><div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf back-color-img-rf">
+                            <h6>Standard</h6>           
+                            <div class="position-relative">
+                                <h3 class="d-inline-block">119</h3>
+                                <span class="dollar-pos">$</span>
+                            </div>
+                            <p class="fs-10-rf lh-lg color-sc-txt-rf mb-4">
+                                10 design concepts <br>
+                                Full copyright ownership <br>
+                                Art direction <br>
+                                Top level creatives <br>
+                                No money back 
+                            </p>
+                            <button class="btn-rf rounded-pill back-color-img-rf mb-5 fs-10-rf"> Learn more <i class="fa-solid fa-caret-right align-middle"></i> </button>
+                        </div>
+                        <div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf borders-colors-button2-rf">
+                            <h6>Basic</h6>           
+                            <div class="position-relative">
+                                <h3 class="d-inline-block">139</h3>
+                                <span class="dollar-pos">$</span>
+                            </div>
+                            <p class="fs-10-rf  lh-lg color-sc-txt-rf mb-4">
+                                15 design concepts <br>
+                                Full copyright ownership <br>
+                                Art direction <br>
+                                Top level creatives <br>
+                                No money back 
+                            </p>
+                            <button class="btn-rf rounded-pill mb-5 fs-10-rf"> Learn more <i class="fa-solid fa-caret-right align-middle"></i> </button>
+                        </div>
                 </div>
-                <div class="col-3 pt-5 pb-5 text-center card opacity-rf card-rf borders-colors-button2-rf">
-                    <h6>Basic</h6>           
-                    <div class="position-relative">
-                        <h3 class="d-inline-block">139</h3>
-                        <span class="dollar-pos">$</span>
-                    </div>
-                    <p class="fs-10-rf  lh-lg color-sc-txt-rf mb-4">
-                        15 design concepts <br>
-                        Full copyright ownership <br>
-                        Art direction <br>
-                        Top level creatives <br>
-                        No money back 
-                    </p>
-                    <button class="btn-rf rounded-pill mb-5 fs-10-rf"> Learn more <i class="fa-solid fa-caret-right align-middle"></i> </button>
-                </div>
-            </div>
+        <a href="#" class="arrow-up-rf pos-arrow-rf"> </a>
+        <div class="pos-icons-rf">
+            <img src="../../src/assets/svg/svg-2.svg" alt="icon" class="icon1-rf">
+            <img src="../../src/assets/svg/svg-4.svg" alt="icon" class="icon2-rf d-block" >
+        </div>
         </div>
     </main>
 </template>
@@ -183,5 +198,46 @@ export default {
             top: 0;
         }
 
+    .pos-arrow-rf {
+        position: absolute;
+        top: 250px;
+        right: 10px;
+        }
+
+        .pos-arrow-2-rf {
+        position: absolute;
+        top: 250px;
+        right: 10px;
+        }
+
+        .icon1-rf {
+            background-color: hsl(347deg 86% 55%);
+            width: 20px;
+            padding: 3px;
+        }
+
+        .icon2-rf {
+            background-color: white;
+            width: 20px;
+            padding: 6px;
+        }
         
+        .pos-icons-rf {
+            position: absolute;
+            right: 0;
+            top: 300px;
+        }
+
+        .icons {
+            font-size: 10px;
+            
+            display: flex;
+            flex-direction: column;
+            position: absolute;
+            right: 20px;
+            top: 100px;
+            i {
+                margin: 10px 0;
+            }
+        }
 </style>
